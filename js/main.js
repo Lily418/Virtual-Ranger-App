130b5f36ea7e31dbaaa96e65fbdf5f8c7f602b07
Lily Hoskin <Lily418@users.noreply.github.com>
2017-10-07 20:55:00 +0100

    Add icons and manifest

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ Notification.requestPermission(function(result) {
   if (result === 'granted') {
     navigator.serviceWorker.ready.then(function(registration) {
       registration.showNotification('We have a mission for you!', {
-        requireInteraction: true
+        requireInteraction: true,
+        badge: '../img/icon.png',
+        icon: '../img/icon.png',
       });
     });
   }
