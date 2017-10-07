4454060d0f7d2c7ffd16d0751032ef12f33931d5
Katerina <katerinaldg@gmail.com>
2017-10-07 19:26:26 +0100

    Add push notifications

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log(Notification.permission)
+navigator.serviceWorker.register('sw.js');
+Notification.requestPermission(function(result) {
+  if (result === 'granted') {
+    navigator.serviceWorker.ready.then(function(registration) {
+      registration.showNotification('We have a mission for you!', {
+        requireInteraction: true
+      });
+    });
+  }
+});
