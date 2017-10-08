d1b303b73570d04667d631e29bfb0a60dd740a8f
Lily Hoskin <Lily418@users.noreply.github.com>
2017-10-08 01:37:19 +0100

    huh

--- sw.js
+++ sw.js
@@ -1,19 +1,9 @@
-// serviceworker.js
 self.addEventListener('notificationclick', function(event) {
-    // Close notification.
-    event.notification.close();
+  console.log('[Service Worker] Notification click Received.');
 
-    // Example: Open window after 3 seconds.
-    // (doing so is a terrible user experience by the way, because
-    //  the user is left wondering what happens for 3 seconds.)
-    var promise = new Promise(function(resolve) {
-        setTimeout(resolve, 3000);
-    }).then(function() {
-        // return the promise returned by openWindow, just in case.
-        // Opening any origin only works in Chrome 43+.
-        return clients.openWindow('/');
-    });
+  event.notification.close();
 
-    // Now wait for the promise to keep the permission alive.
-    event.waitUntil(promise);
+  event.waitUntil(
+    clients.openWindow('https://virtual-ranger.site/')
+  );
 });
